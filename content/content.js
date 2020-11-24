0163d44b83b0a792299d05dd46760299cf1b9cc0
Jeong-Hyun-Su <mses1572@naver.com>
2020-11-24 18:40:38 +0900

    modify [content] event

--- content/content.js
+++ content/content.js
@@ -15,6 +15,7 @@ myPort.onMessage.addListener(function(msg){
     }
 });
 
+// 우클릭 Custom Menu
 $(document).bind("contextmenu", function(event) { 
         event.preventDefault();
         
@@ -31,34 +32,10 @@ $(document).bind("contextmenu", function(event) {
         
 })
 .bind("click", function(event) {
-        
         $("div.custom-menu").hide();
 });
 
-
-
-// document.body.addEventListener("mousedown", function(event){
-//     // Drage && Right mouse button
-//     if((event.button == 2) || (event.which == 3)){
-//         event.preventDefault();
-//         let getText = selectText().trim();
-        
-//         if(getText != ""){
-//             myPort.postMessage({addText: getText});
-//         }
-//     }
-// });
-
-$(document).bind("contextmenu", function(event) { 
-    event.preventDefault();
-    $("<div class='custom-menu'>Custom menu</div>")
-        .appendTo("body")
-        .css({top: event.pageY + "px", left: event.pageX + "px"});
-}).bind("click", function(event) {
-    $("div.custom-menu").hide();
-});
-
-
+// 드래그된 텍스트
 function selectText() {
     var selectText = "";
     
@@ -74,34 +51,38 @@ function selectText() {
     return String(selectText);
 }
 
-// Get - hover html element
-// let x = event.clientX, 
-//     y = event.clientY,
-//     elementMouseIsOver = document.elementFromPoint(x, y);
+/*  마우스에 위치하는 HTML 요소를 가져와 PLUS 버튼 생성해서 붙이기.
+    
 
-// let getElement = elementMouseIsOver.getElementsByClassName("ainize_logo");
-// let getText = elementMouseIsOver.innerText;
-// let hasAinizeLogo = false;
+    Get - hover html element
+    let x = event.clientX, 
+        y = event.clientY,
+        elementMouseIsOver = document.elementFromPoint(x, y);
 
-// // Check - 단락 Ainize Logo 
-// for(let i=0; i<getElement.length; i++){
-//     if(getElement[i].className == "ainize_logo")
-//             hasAinizeLogo = true;
-// }
+    let getElement = elementMouseIsOver.getElementsByClassName("ainize_logo");
+    let getText = elementMouseIsOver.innerText;
+    let hasAinizeLogo = false;
 
-// // 로고 없으면, Insert Ainize Logo
-// if(hasAinizeLogo == false){
-//     let logo = document.createElement("img");
+    // Check - 단락 Ainize Logo 
+    for(let i=0; i<getElement.length; i++){
+        if(getElement[i].className == "ainize_logo")
+                hasAinizeLogo = true;
+    }
 
-//     logo.className = "ainize_logo";
-//     logo.src = chrome.extension.getURL("plus.png");
-//     logo.style.cursor = 'pointer';
+    // 로고 없으면, Insert Ainize Logo
+    if(hasAinizeLogo == false){
+        let logo = document.createElement("img");
 
-//     // 단락의 마지막 IMG 추가
-//     elementMouseIsOver.appendChild(logo);
+        logo.className = "ainize_logo";
+        logo.src = chrome.extension.getURL("plus.png");
+        logo.style.cursor = 'pointer';
 
-//     logo.onclick = function(){
-//         let text = getText;
-//         myPort.postMessage({addText: text});
-//     };
-// }
+        // 단락의 마지막 IMG 추가
+        elementMouseIsOver.appendChild(logo);
+
+        logo.onclick = function(){
+            let text = getText;
+            myPort.postMessage({addText: text});
+        };
+    }
+*/
